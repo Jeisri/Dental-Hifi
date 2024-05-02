11fd52933e441e862b40ddb279280a8b4d31c9b1
nav2924 <navi29012004@gmail.com>
2024-05-02 21:13:35 +0530

    API endpoint Login/Register

--- backend/Server.js
+++ backend/Server.js
@@ -1,12 +1,29 @@
 import express from "express";
+import colors from "colors";
+import morgan from "morgan";
 import dotenv from "dotenv";
-const app = express();
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
-// const 5000 = process.env.5000 || 5000;
-app.get("/",(req,res) => {
-    res.send("Server is ready");
-})
-app.listen(5000 , () => 
-    console.log("Server is running on port 5000")
-);
+
+connectDB();
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/v1/user", userRoutes);
+
+// Port
+const port = process.env.PORT || 5000;
+
+// Listen port
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${port}`
+  );
+});
